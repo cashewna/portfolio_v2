f293a6b65b76b785c82bf0cb14718a57891ef3df
cashewna <krishnakhong@gmail.com>
2022-08-12 16:06:06 +0800

    Added socials

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./header.css";
+import CTA from "./CTA";
+import ME from "../../assets/me.jpeg";
+import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
   return (
@@ -8,6 +11,16 @@ const Header = () => {
         <h5>Hello I'm</h5>
         <h1>Krishna Khong</h1>
         <h5 className="text-light">Student | Aspiring Software Developer</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className="scroll_down">
+          Scroll Down
+        </a>
       </div>
     </header>
   );
