d35dae0eb62869fcf9f9ab34d2f9e1625204ac33
cashewna <krishnakhong@gmail.com>
2022-08-13 00:57:37 +0800

    added navigation icons

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import './nav.css'
+import React from "react";
+import "./nav.css";
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { BiBook } from "react-icons/bi";
+import { FaLaptopCode } from "react-icons/fa";
+import { BsChatLeftText } from "react-icons/bs";
 
 const Nav = () => {
   return (
-    <div>Nav</div>
-  )
-}
+    <nav>
+      <a href="#">
+        <AiOutlineHome />
+      </a>
+      <a href="#about">
+        <AiOutlineUser />
+      </a>
+      <a href="#experience">
+        <BiBook />
+      </a>
+      <a href="#portfolio">
+        <FaLaptopCode />
+      </a>
+      <a href="#contact">
+        <BsChatLeftText />
+      </a>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
