402f9c524b5f3c275bd676792d07ac5979934eee
cashewna <krishnakhong@gmail.com>
2022-08-18 11:05:04 +0800

    Added background and hover effects on experience components and fixed divs

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -15,45 +15,68 @@ const Experience = () => {
           <div className="experience__content">
             <article className="experience__details">
               <BsPatchCheckFill />
-              <h4>HTML5</h4>
-              <small className="text-light">Beginner</small>
+              <div>
+                <h4>HTML5</h4>
+                <small className="text-light">Beginner</small>
+              </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill />
-              <h4>CSS3</h4>
-              <small className="text-light">Beginner</small>
+              <div>
+                <h4>CSS3</h4>
+                <small className="text-light">Beginner</small>
+              </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill />
-              <h4>JavaScript</h4>
-              <small className="text-light">Beginner</small>
+              <div>
+                <h4>JavaScript</h4>
+                <small className="text-light">Beginner</small>
+              </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill />
-              <h4>Python</h4>
-              <small className="text-light">Beginner</small>
+              <div>
+                <h4>Python</h4>
+                <small className="text-light">Beginner</small>
+              </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill />
-              <h4>Java</h4>
-              <small className="text-light">Beginner</small>
+              <div>
+                <h4>Java</h4>
+                <small className="text-light">Beginner</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <div>
+                <h4>MySQL</h4>
+                <small className="text-light">Beginner</small>
+              </div>
             </article>
           </div>
         </div>
+        {/* End of Programming Languages */}
 
-        <div className="experience__techstacks"></div>
-        <h3>Tech Stacks</h3>
-        <div className="experience__content">
-          <article className="experience__details">
-            <BsPatchCheckFill />
-            <h4>TailwindCSS</h4>
-            <small className="text-light">Beginner</small>
-          </article>
-          <article className="experience__details">
-            <BsPatchCheckFill />
-            <h4>ReactJS</h4>
-            <small className="text-light">Beginner</small>
-          </article>
+        <div className="experience__techstacks">
+          <h3>Tech Stacks</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <div>
+                <h4>TailwindCSS</h4>
+                <small className="text-light">Beginner</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <div>
+                <h4>ReactJS</h4>
+                <small className="text-light">Beginner</small>
+              </div>
+            </article>
+          </div>
         </div>
       </div>
     </section>
