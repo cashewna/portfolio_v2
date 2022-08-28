c1e57867f7e8183de6e61993bd6e979e1bc3cee2
cashewna <krishnakhong@gmail.com>
2022-08-28 19:05:29 +0800

    Revert breaking swiper module

--- src/components/quotes/Quotes.jsx
+++ src/components/quotes/Quotes.jsx
@@ -15,8 +15,7 @@ const data = [
   {
     avatar: AUTHOR1,
     name: "Master Oogway",
-    quote:
-      "If you only do what you can do, you will never be more than you are now.",
+    quote: "If you only do what you can do, you will never be more than you are now.",
   },
   {
     avatar: AUTHOR2,
@@ -25,8 +24,7 @@ const data = [
       "I fear not the man who has practiced 10,000 kicks once, but I fear the man who has practiced one kick 10,000 times.",
     avatar: AUTHOR3,
     name: "Terry Pratchett",
-    quote:
-      "Build a man a fire, he is warm for a night. Set a man on fire, he is warm for the rest of his life.",
+    quote: "Build a man a fire, he is warm for a night. Set a man on fire, he is warm for the rest of his life.",
   },
   {
     avatar: AUTHOR4,
